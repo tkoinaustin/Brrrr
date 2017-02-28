bbb2291449e2230ffe6ec93a4f0682b6c43e34c8
Tom Nelson <tkoinaustin@yahoo.com>
2017-02-27 21:16:09 -0600

    swiftlint and fabric

--- Brrrr/AppDelegate.swift
+++ Brrrr/AppDelegate.swift
@@ -7,14 +7,19 @@
 //
 
 import UIKit
+import Fabric
+import Crashlytics
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
   var window: UIWindow?
 
+  func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    Fabric.with([Answers.self, Crashlytics.self])
 
-  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
     return true
   }
 
@@ -32,7 +37,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   func applicationWillTerminate(_ application: UIApplication) {
   }
-
-
 }
-
---
+++ Podfile
@@ -0,0 +1,12 @@
+platform :ios, '10.0'
+use_frameworks!
+
+target 'Brrrr' do
+  pod 'SwiftLint'
+  pod 'Fabric'
+  pod 'Crashlytics'
+
+  target 'BrrrrTests' do
+  end
+end
+
