79a29162353e8f06ce6fe6496cf79d53d78576e3
Tom Nelson <tom.nelson@thirteen23.com>
2017-05-24 13:44:25 -0500

    Made DarkSkyResponse data structure an observable

--- Brrrr/ViewController.swift
+++ Brrrr/ViewController.swift
@@ -8,19 +8,53 @@
 
 import UIKit
 import PromiseKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
+  let disposeBag = DisposeBag()
+
+  @IBOutlet weak var displayLabel: UILabel!
+  
+  @IBAction func buttonAction(_ sender: UIButton) {
+
+  }
+  
+  func dataRequest() -> Observable<DarkSkyResponse> {
+    return Observable.create { observer in
+      DarkSkyResponse.loadPromise().then { darkSkyData -> Void in
+        observer.onNext(darkSkyData)
+        observer.onCompleted()
+      }
+      return Disposables.create()
+    }
+  }
+  
+  func bind() {
+    dataRequest()
+      .observeOn(MainScheduler.instance)
+      .debug("dataRequest")
+      .subscribe(onNext: { weatherData in
+        
+        let dateString = weatherData.dateFormatter.string(from: (weatherData.minutely?.item[0].time)!)
+        print(dateString)
+        for item in (weatherData.minutely?.item)! {
+          print ("Minutely item \(weatherData.dateFormatter.string(from: (item.time)!))")
+        }
+        for item in (weatherData.hourly?.item)! {
+          print ("Hourly item \(weatherData.dateFormatter.string(from: (item.time)!)) \(weatherData.tempFormatter.string(from: item.apparentTemperature) ?? item.apparentTemperature.description)")
+        }
+        for item in (weatherData.daily?.item)! {
+          print ("Daily item \(weatherData.dateFormatter.string(from: (item.time)!)) \(String(describing: weatherData.tempFormatter.string(from: item.apparentTemperatureMin)!))/\(weatherData.tempFormatter.string(from: item.apparentTemperatureMax)!)")
+        }
+      })
+    .addDisposableTo(disposeBag)
+  }
   
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    //    let data = DarkSkyResponse.load()
-    DarkSkyResponse.loadPromise().then { darkSkyData in
-//      print("\(darkSkyData)")
-      print("\(darkSkyData.minutely?.summary)")
-    }
-    //      if let currentTemp = darkSkyData["currently"]["apparentTemperature"].string {
-    //        print("temperature is \(currentTemp)")
+    bind()
   }
   
 }
