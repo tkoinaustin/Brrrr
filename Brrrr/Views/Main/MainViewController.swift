05fc15bbff01107b97706cd032aa419477a99003
Tom Nelson <tom.nelson@thirteen23.com>
2017-05-24 11:19:07 -0500

    Added Views group with Main subgroup for first view controller

---
+++ Brrrr/Views/Main/MainViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MainViewController.swift
+//  Brrrr
+//
+//  Created by Tom Nelson on 5/24/17.
+//  Copyright © 2017 TKO Solutions. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+  
+}
