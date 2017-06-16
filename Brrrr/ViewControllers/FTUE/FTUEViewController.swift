b5526e2c087af0ae05910a093e8daa2f1527e691
Tom Nelson <tom.nelson@thirteen23.com>
2017-06-16 14:00:53 -0500

    Added FTUE framework

---
+++ Brrrr/ViewControllers/FTUE/FTUEViewController.swift
@@ -0,0 +1,21 @@
+//
+//  FTUEViewController.swift
+//  Brrrr
+//
+//  Created by Tom Nelson on 6/16/17.
+//  Copyright © 2017 TKO Solutions. All rights reserved.
+//
+
+import UIKit
+
+class FTUEViewController: UIViewController {
+  
+  @IBOutlet private weak var panelsView: UIScrollView!
+  @IBOutlet private weak var pageControll: UIPageControl!
+  @IBOutlet private var panelOne: UIView!
+  @IBOutlet private var panelTwo: UIView!
+  @IBOutlet private var panelThree: UIView!
+  @IBOutlet private var panelFour: UIView!
+  
+  
+}
